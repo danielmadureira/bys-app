a8f006e89db77cf873c1e0d89538d0a78a88cb4d
renataandrade <knattly@gmail.com>
2020-11-13 00:45:24 -0300

    feat: adicionado Crousel ao feed

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,63 +1,193 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
-import { FlatList, ScrollView } from 'react-native-gesture-handler';
+import {
+	View,
+	SectionList,
+	SafeAreaView,
+	FlatList,
+} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import { CardImage, TitleHeader } from '../../components';
 
-import { styles } from "./styles";
+import { styles } from './styles'
 import ImagemSVG from '../../../assets/Feed-Example.jpg'
-import ButtonBase from '../../components/base/ButtonBase';
+import HorizontalFirst from '../../../assets/Horizontal.jpg'
 
-const DATA = [
-	{
-		id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-		title: 'Orientações nutricionais no COVID-19',
-		subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
-		picture: ImagemSVG,
-	},
-	{
-		id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-		title: 'Orientações nutricionais no COVID-19',
-		subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
-		picture: ImagemSVG,
-	},
-	{
-		id: '58694a0f-3da1-471f-bd96-145571e29d72',
-		title: 'Orientações nutricionais no COVID-19',
-		subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
-		picture: ImagemSVG,
-	},
-];
 
-const Feed = (props) => {
-	const { history } = props;
+const Feed = () => {
 	return (
-		<ScrollView contentContainerStyle={styles.container}>
+		<ScrollView contentContainerStyle={styles.wrapper} style={styles.container}>
+			<StatusBar style="light" backgroundColor="#000" />
 			<View style={styles.container_header}>
-				<TitleHeader 
+				<TitleHeader
 					title="Feed"
 					subtitle="Veja as últimas novidades"
 				/>
 			</View>
-			<ButtonBase
-				title="Voltar"
-				onPress={() => {
-					history.goBack()
-				}}
-			/>
-
-			{/** begin::news */}
-			<SafeAreaView>
-				<FlatList
-					data={DATA}
-					renderItem={({ item }) => (
-						<CardImage item={item} />
+			<SafeAreaView style={{ flex: 1 }}>
+				<SectionList
+					stickySectionHeadersEnabled={false}
+					sections={SECTIONS}
+					renderSectionHeader={({ section }) => (
+						<>
+							{section.horizontal ? (<View style={styles.wrapper}>
+								<View style={styles.container_header}>
+									<TitleHeader
+										title={section.title}
+										subtitle="Veja as últimas novidades"
+									/>
+								</View>
+								<FlatList
+									horizontal
+									data={section.data}
+									renderItem={({ item }) => <CardImage smallSize item={item} />}
+									showsHorizontalScrollIndicator={false}
+								/>
+							</View>
+							) : null}
+						</>
 					)}
-					keyExtractor={item => item.id}
+					renderItem={({ item, section }) => {
+						if (section.horizontal) {
+							return null;
+						}
+						return <View style={styles.wrapper}>
+							<CardImage item={item} />
+						</View>;
+					}}
 				/>
 			</SafeAreaView>
-			{/** end::news */}
 		</ScrollView>
-	)
-}
+	);
+};
+
+const SECTIONS = [
+	{
+		title: 'Entretenimento',
+		data: [
+			{
+				key: '1',
+				text: 'Item text 1',
+				uri: 'https://picsum.photos/id/1/200',
+				id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+				title: 'Orientações nutricionais no COVID-19',
+				subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
+				picture: ImagemSVG,
+			},
+			{
+				key: '2',
+				text: 'Item text 2',
+				uri: 'https://picsum.photos/id/10/200',
+				id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+				title: 'Orientações nutricionais no COVID-19',
+				subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
+				picture: ImagemSVG,
+			}
+		],
+	},
+	{
+		title: 'Entretenimento',
+		horizontal: true,
+		data: [
+			{
+				key: '1',
+				text: 'Item text 1',
+				uri: 'https://picsum.photos/id/1011/200',
+				id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+				title: 'Orientações nutricionais no COVID-19',
+				subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
+				picture: HorizontalFirst,
+			},
+			{
+				key: '2',
+				text: 'Item text 2',
+				uri: 'https://picsum.photos/id/1012/200',
+				id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+				title: 'Orientações nutricionais no COVID-19',
+				subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
+				picture: HorizontalFirst,
+			},
+
+			{
+				key: '3',
+				text: 'Item text 3',
+				uri: 'https://picsum.photos/id/1013/200',
+				id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+				title: 'Orientações nutricionais no COVID-19',
+				subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
+				picture: HorizontalFirst,
+			},
+			{
+				key: '4',
+				text: 'Item text 4',
+				uri: 'https://picsum.photos/id/1015/200',
+				id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+				title: 'Orientações nutricionais no COVID-19',
+				subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
+				picture: HorizontalFirst,
+			},
+			{
+				key: '5',
+				text: 'Item text 5',
+				uri: 'https://picsum.photos/id/1016/200',
+				id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+				title: 'Orientações nutricionais no COVID-19',
+				subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
+				picture: ImagemSVG,
+			},
+		],
+	},
+	{
+		title: 'Based on your recent listening',
+		data: [
+			{
+				key: '1',
+				text: 'Item text 1',
+				uri: 'https://picsum.photos/id/1020/200',
+				id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+				title: 'Orientações nutricionais no COVID-19',
+				subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
+				picture: ImagemSVG,
+			},
+			{
+				key: '2',
+				text: 'Item text 2',
+				uri: 'https://picsum.photos/id/1024/200',
+				id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+				title: 'Orientações nutricionais no COVID-19',
+				subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
+				picture: ImagemSVG,
+			},
+
+			{
+				key: '3',
+				text: 'Item text 3',
+				uri: 'https://picsum.photos/id/1027/200',
+				id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+				title: 'Orientações nutricionais no COVID-19',
+				subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
+				picture: ImagemSVG,
+			},
+			{
+				key: '4',
+				text: 'Item text 4',
+				uri: 'https://picsum.photos/id/1035/200',
+				id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+				title: 'Orientações nutricionais no COVID-19',
+				subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
+				picture: ImagemSVG,
+			},
+			{
+				key: '5',
+				text: 'Item text 5',
+				uri: 'https://picsum.photos/id/1038/200',
+				id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+				title: 'Orientações nutricionais no COVID-19',
+				subtitle: 'Larissa Menezes Santos - Nutricionista, especialista em Terapia Nutricional',
+				picture: ImagemSVG,
+			},
+		],
+	},
+];
 
 export default Feed;
--- src/pages/Feed/styles.js
+++ src/pages/Feed/styles.js
@@ -2,13 +2,31 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
 	container: {
+		flex: 1,
+		paddingTop: 45,
+	},
+	wrapper: {
 		display: "flex",
-		justifyContent: "center",
-		paddingLeft: 20,
-		paddingRight: 20,
-		marginTop: 68,
+		alignItems: "center",
 	},
 	container_header: {
+		marginTop: 10,
 		paddingBottom: 2,
-	}
+		width: 320
+	},
+	sectionHeader: {
+		fontWeight: '800',
+		fontSize: 18,
+		marginTop: 20,
+	},
+	item: {
+		margin: 10,
+	},
+	itemPhoto: {
+		width: 200,
+		height: 200,
+	},
+	itemText: {
+		marginTop: 5,
+	},
 });
