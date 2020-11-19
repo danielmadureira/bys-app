3dca678ee74f858216584647b3d0cdc98a487d61
renataandrade <knattly@gmail.com>
2020-11-18 22:41:49 -0300

    feat: criado página de conversas

---
+++ src/pages/Message/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingTop: 45,
+	},
+	wrapper: {
+		display: "flex",
+		alignItems: "center",
+	},
+	container_header: {
+		marginTop: 10,
+		paddingBottom: 15,
+		width: 320
+	},
+	wrapper_card: {
+		marginTop: 5,
+		margin: 20
+	}
+});
