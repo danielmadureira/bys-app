e1339db8c33956a9fdba84e317582e434dd5b53a
renataandrade <knattly@gmail.com>
2020-11-18 22:35:41 -0300

    feat: exportando páginas

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,15 @@ export { default as Login } from './Login/Login';
 
 // Página de feeds
 export { default as Feed } from './Feed/Feed';
+
+// Página Diário
+export { default as Diary } from './Diary/Diary'
+
+// Página de Conversas
+export { default as Message } from './Message/Message'
+
+// Página de Notificações
+export { default as Notification } from './Notification/Notification'
+
+// Página de Calculadoras
+export { default as Calculator } from './Calculator/Calculator'
