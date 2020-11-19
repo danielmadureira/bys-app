f4cda7762cb7978152a5984c7dc3cb0d2271660d
renataandrade <knattly@gmail.com>
2020-11-18 22:33:29 -0300

    feat: criado rotas das páginas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,29 +1,91 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { NativeRouter, Route } from 'react-router-native';
-import Stack from 'react-router-native-stack';
 
-import { Feed, Login } from '../pages';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { 
+	Feed, 
+	Login, 
+	Diary, 
+	Message, 
+	Notification,
+	Calculator
+} from '../pages';
+
 import { Main } from '../components';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const RoutesTabs = () => {
+	return (
+		<Tab.Navigator initialRouteName="Feed" tabBar={props => <Main {...props} />} animationType="slide-horizontal">
+			<Tab.Screen
+				name="Feed"
+				component={Feed}
+				options={{
+					headerShown: false
+				}}
+			/>
+
+			<Tab.Screen
+				name="Diary"
+				component={Diary}
+				options={{
+					headerShown: false
+				}}
+			/>
+
+			<Tab.Screen
+				name="Message"
+				component={Message}
+				options={{
+					headerShown: false
+				}}
+			/>
+			
+			<Tab.Screen
+				name="Calculator"
+				component={Calculator}
+				options={{
+					headerShown: false
+				}}
+			/>
+			
+			<Tab.Screen
+				name="Notification"
+				component={Notification}
+				options={{
+					headerShown: false
+				}}
+			/>
+		</Tab.Navigator>
+	)
+}
 
 const Routes = () => {
 	return (
-		<NativeRouter>
-			<Stack animationType="slide-horizontal">
-				<Route
-					exact
-					path="/"
+		<NavigationContainer>
+			<Stack.Navigator animationType="slide-horizontal">
+				<Stack.Screen
+					name="Home"
 					component={Login}
+					options={{
+						headerShown: false
+					}}
 				/>
 
-				<Route
-					exact
-					path="/feed"
-					component={Feed}
-					footerComponent={Main}
+				<Stack.Screen
+					name="Feed"
+					component={RoutesTabs}
+					options={{
+						headerShown: false
+					}}
 				/>
-			</Stack>
-		</NativeRouter>
+			</Stack.Navigator>
+		</NavigationContainer>
 	)
 }
 
