cc9588359b393ba11f0e35f649f5f2faee3b79b9
renataandrade <knattly@gmail.com>
2021-03-03 00:08:41 -0300

    feat: criado serviço de usuário e integrado à api

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Image } from 'react-native';
 import { ScrollView, TextInput } from 'react-native-gesture-handler';
 
 import { styles } from './styles';
 import LogoPNG from '../../../assets/logo.png';
 import ButtonBase from '../../components/base/ButtonBase';
+import { useDispatch } from 'react-redux';
+import { actions } from '../../store/user';
+import { AuthServices } from '../../services/AuthServices';
+import { SecureStoreServices } from '../../services/SecureStoreServices';
 
 const Login = (props) => {
-	const { navigation  } = props;
+	const { navigation } = props
+	const dispatch = useDispatch()
+
+	const [email, setEmail] = useState('administrator@bys-app.com')
+	const [password, setPassword] = useState('123')
+
+	const login = () => {
+		SecureStoreServices.deleteItemAsync('_token')
+		AuthServices.authenticate({
+			email,
+			password: 'c19760e4c50be2ef8b7c479d2a8cc67e230659e5625ddb31b03ceabc15eea849',
+			device_name: 'emulator_client'
+		})
+		.then(async (res) => {
+			await SecureStoreServices.setItemAsync('_token', res.plainTextToken)
+
+			dispatch(actions.getUser())
+			navigation.navigate('Feed')
+		})
+		.catch(err => console.log(err, 'Error'))
+	}
 
 	return (
 		<ScrollView contentContainerStyle={styles.container}>
@@ -27,18 +51,20 @@ const Login = (props) => {
 				<TextInput
 					placeholder="E-mail"
 					style={styles.input}
+					value={email}
+					onChangeText={(email) => setEmail(email)}
 				/>
 				<TextInput
 					placeholder="Senha"
 					secureTextEntry={true}
 					style={styles.input}
+					value={password}
+					onChangeText={(password) => setPassword(password)}
 				/>
 				<View style={styles.wrapper_button}>
 					<ButtonBase
 						title="Entrar"
-						onPress={() => {
-							navigation.navigate('Feed')
-						}}
+						onPress={() => { login() }}
 					/>
 
 					<Text style={styles.textHelper}>
