588b8bdcf08ab8d96de36e67975322a9af96ac31
renataandrade <knattly@gmail.com>
2021-03-28 20:15:21 -0300

    feat: criado helpers de notificação

---
+++ src/helpers/notificationHelper.js
@@ -0,0 +1,68 @@
+import { WEEK } from "../enums/Week"
+
+/**
+ * Convert and return just 
+ * the number of week
+ * 
+ * @param {array} week 
+ * @returns {array}
+ */
+const convertWeek = (
+  week
+) => {
+  let days = []
+
+  week.map((day) => {
+    if (day.selected) {
+      days.push(day.id)
+    }
+  })
+
+  return days
+}
+
+/**
+ * Name of days 
+ * of week
+ */
+const daysInString = [
+  WEEK.NAME.SUNDAY,
+  WEEK.NAME.MONDAY,
+  WEEK.NAME.TUESDAY,
+  WEEK.NAME.WEDNESDAY,
+  WEEK.NAME.THURSDAY,
+  WEEK.NAME.FRIDAY,
+  WEEK.NAME.SATURDAY
+]
+
+/**
+ * Transform numbers of day
+ * of week into name of day, 
+ * and returns one string
+ * 
+ * @param {array} days
+ * @returns {string}
+ */
+const convertDaysToString = (days) => {
+  let daysName = []
+  days.map(v => {
+    daysName.push(daysInString[v])
+  })
+  return daysName.join('; ')
+}
+
+/**
+ * Transform array in string
+ * 
+ * @param {array} hours 
+ * @returns {string}
+ */
+const convertHoursToString = (hours) => {
+  return hours.join('/')
+}
+
+export const NotificationHelper = {
+  convertWeek,
+  convertDaysToString,
+  convertHoursToString
+}
